035a27069c6993c06ba7ff71c3fa2a0784142bf7
Solumilken <s916526000@gmail.com>
2017-02-03 17:42:34 +0800

    add setup.xxx

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+install_requires = [
+    'xgboost',
+    'numpy',
+]
+
+setup(
+    name='xgboostwithwarmstart',
+    version="0.1.1",
+    description='XGBoost can warm start !!!!',
+    long_description=("See `github <'https://github.com/SoluMilken/xgboostwithwarmstart'>`_ "
+                      "for more information."),
+    author='ianlini and solumilken',
+    url='https://github.com/SoluMilken/xgboostwithwarmstart',
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=[
+        'xgboostwithwarmstart',
+    ],
+    package_dir={
+        'xgboostwithwarmstart': 'xgboostwithwarmstart',
+    },
+)
